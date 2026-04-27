ea09a9a9c2266b8bf74dd50ea1ad2d9815645660
Arthur Caumartin <artcaumart@gmail.com>
2026-04-28 01:13:21 +0200

    create folder for projectile decorator

--- TD/Assets/[Project]/Scripts/Decorator/Projectile/ProjectileDecoratorExplosionDelay.cs
+++ TD/Assets/[Project]/Scripts/Decorator/Projectile/ProjectileDecoratorExplosionDelay.cs
@@ -1,20 +1,4 @@
-using System;
 using UnityEngine;
-
-[Serializable]
-public class ProjectileDecoratorSwing : ProjectileDecoratorBase
-{
-    public override void ComposableUpdate()
-    {
-        base.ComposableUpdate();
-        transform.Translate(Vector2.right * Time.deltaTime * Mathf.Cos(Time.time * 10) * 5);
-    }
-
-    public ProjectileDecoratorSwing(Composable composable, ProjectileDecoratorBase projectileDecoratorBase)
-    : base(composable, projectileDecoratorBase)
-    {
-    }
-}
 
 public class ProjectileDecoratorExplosionDelay : ProjectileDecoratorBase
 {
