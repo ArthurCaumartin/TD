247d5924de2fc2d3e53534f110050f03047d79fe
Arthur Caumartin <artcaumart@gmail.com>
2026-05-04 19:48:19 +0200

    Add Array utils GetNearest

--- TD/Assets/[Project]/Scripts/Utils/ArrayUtils.cs
+++ TD/Assets/[Project]/Scripts/Utils/ArrayUtils.cs
@@ -6,4 +6,20 @@ public static class ArrayUtils
     {
         return array[Random.Range(0, array.Length)];
     }
+
+    public static T GetNearset<T>(this T[] array, Vector3 position) where T : MonoBehaviour
+    {
+        T nearest = null;
+        float distance = Mathf.NegativeInfinity;
+        for (int i = 0; i < array.Length; i++)
+        {
+            float currentDistance = (array[i].transform.position - position).sqrMagnitude;
+            if(distance < currentDistance)
+            {
+                distance = currentDistance;
+                nearest = array[i];
+            }
+        }
+        return nearest;
+    }
 }
